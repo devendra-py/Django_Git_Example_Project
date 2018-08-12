71f7a9a9f3ed8a8a6749993f153de32d7e9627b3
devendra5 <35335412+devendra5@users.noreply.github.com>
2018-08-12 23:10:10 +0530

    Initial commit

---
+++ GitApp/models.py
@@ -0,0 +1,7 @@
+from django.db import models
+
+# Create your models here.
+
+
+# here model classess are created by extending models.Model
+# this comment section ........... we are applied .
---
+++ GitApp/views.py
@@ -0,0 +1,7 @@
+from django.shortcuts import render
+
+from django.http import HttpResponse
+def Git(request):
+    return HttpResponse("<h1>this is sample Git Project</h1>")
+
+# we are .. devoloping some automation Project ...
